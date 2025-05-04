3e83bce292779da1155534105649f90c056e8fbd
Asaf Gery <asaf.gery@gmail.com>
2025-05-04 07:18:26 +0300

    initial commit

---
+++ src/main/java/org/yosefdreams/YosefDreamsApplication.java
@@ -0,0 +1,13 @@
+package org.yosefdreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosefDreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosefDreamsApplication.class, args);
+	}
+
+}
