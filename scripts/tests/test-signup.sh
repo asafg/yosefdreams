dfaf2f2c42a77022b45b1e5ab73c1275a33584da
Asaf Gery <asaf.gery@gmail.com>
2025-05-16 00:57:02 +0300

    test-signup.sh now accepts params from command line.

--- scripts/tests/test-signup.sh
+++ scripts/tests/test-signup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+username="${1:-'admin'}"
+email="${2:-'admin@gmail.com'}"
+password="${3:-'password'}"
+
 echo 'curl -X POST "http://localhost:8080/api/auth/signup" \' >&2
 echo '    -H "Content-Type: application/json" \' >&2
-echo "    -d '{\"username\": \"admin\",\"email\": \"admin@gmail.com\",  \"password\": \"admin\"}'" >&2
+echo "    -d '{\"username\": \"${username}\",\"email\": \"admin@gmail.com\",  \"password\": \"${password}\"}'" >&2
 curl -X POST "http://localhost:8080/api/auth/signup" \
      -H "Content-Type: application/json" \
-     -d '{"username": "admin","email": "admin@gmail.com",  "password": "admin"}'
+     -d '{"username": "${username}","email": "${email}",  "password": "${password}"}'
 
 echo -e '\n'
