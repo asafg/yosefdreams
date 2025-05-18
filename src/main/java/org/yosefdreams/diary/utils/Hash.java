61f2cf0cc72d64f47516737bdde08cb0faf01be3
Asaf Gery <asaf.gery@gmail.com>
2025-05-18 04:03:31 +0300

    Moved hash creation to its own utility class, since it is being used by multiple classes.

---
+++ src/main/java/org/yosefdreams/diary/utils/Hash.java
@@ -0,0 +1,18 @@
+package org.yosefdreams.diary.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Hash {
+
+  /** According the documentation BCryptPasswordEncoder range is between 4 to 31 */
+  private static final int BCRYPT_PASSWORD_ENCODER_STRENGTH = 16;
+
+  public static String hashString(String plainText) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(BCRYPT_PASSWORD_ENCODER_STRENGTH);
+    // According to
+    // https://docs.spring.io/spring-security/site/docs/5.3.0.RELEASE/reference/html5/#authentication-password-storage
+    // a prefix of the password encoder being used should be add to the hash
+    // in order to allow identify the password encoder that created this hash.
+    return "{bcrypt}" + encoder.encode(plainText);
+  }
+}
