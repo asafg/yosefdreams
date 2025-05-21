23b0203bfd6dcbe2d724bd6b13e581a7bbc12357
Asaf Gery <asaf.gery@gmail.com>
2025-05-21 03:36:14 +0300

    Added missing ;

--- src/main/java/org/yosefdreams/diary/repository/UserRepository.java
+++ src/main/java/org/yosefdreams/diary/repository/UserRepository.java
@@ -9,8 +9,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByUsernameOrEmail(String username, String email);
 
-  Optional<User> findByUsername(String username)
-  
+  Optional<User> findByUsername(String username);
+
   Optional<User> findByResetToken(String username);
 
   Optional<User> findByResetToken(String hashedResetToken);
